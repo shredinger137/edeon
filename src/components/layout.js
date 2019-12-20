058b9d8a8672b65ff9697650e32a58c4ca59dc4f
Shredinger137 <caseylewiston@gmail.com>
2019-12-20 09:15:20 -0800

    Test

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import SEO from "./seo"
+import "../css/epo.css"
 
 import Header from "./header"
 import Menu from "./menu"
--- src/templates/single-project.js
+++ src/templates/single-project.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import "../css/epo.css"
 
 export const ProjectTemplate = ({ title, content }) => {
   return (
